0ab83dbdedfa35933ff6d3efb7373418e37f2704
201250203 <1072825625@qq.com>
2022-11-22 18:37:29 +0800

    03fix

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ import java.util.List;
 public class Main
 {    
     public static void main(String[] args) throws IOException {
-//        if (args.length < 1) {
-//            System.err.println("input path is required");
-//        }
-        String source = "/Users/harunzzz/Codes/Lab/src/a.txt";
+        if (args.length < 1) {
+            System.err.println("input path is required");
+        }
+        String source = args[0];
         CharStream input = CharStreams.fromFileName(source);
         SysYLexer sysYLexer = new SysYLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(sysYLexer);
